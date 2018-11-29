ddd12eff0a08c26f9828ff76c7c6477a3fa84820
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-29 13:59:43 -0700

    Test for filtering comedians

--- spec/features/user_sees_filtered_comedians_spec.rb
+++ spec/features/user_sees_filtered_comedians_spec.rb
@@ -10,6 +10,18 @@
 
 RSpec.describe 'As a visitor to the app' do
   it 'filters the comedians on the page to only show those of indicated age' do
+    bob = Comedian.create(name: "Bob", age: 30, city: "Denver")
+    joe = Comedian.create(name: "Joe", age: 34, city: "Stockholm")
+    jane = Comedian.create(name: "Jane", age: 40, city: "Stockholm")
+
+    visit '/comedians?age=34'
     
+    expect(page).to have_css("article#comedian-#{bob.id}")
+    expect(page).to_not have_css("article#comedian-#{joe.id}")
+    expect(page).to_not have_css("article#comedian-#{jane.id}")
+    expect(page).to have_content("#{joe.name}")
+    expect(page).to have_content("Age: #{joe.age}")
+    expect(page).to_not have_content("#{bob.name}")
+    expect(page).to_not have_content("#{jane.name}")
   end
 end
