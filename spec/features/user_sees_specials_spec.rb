09fc69b9d09b0c7c94b5e270f46d4add243c6394
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-28 17:45:30 -0700

    Adds images and runtime to specials

--- spec/features/user_sees_specials_spec.rb
+++ spec/features/user_sees_specials_spec.rb
@@ -1,24 +1,32 @@
-# User Story 2
+# User Story 3
 # 
 # As a visitor
 # When I visit `/comedians`
-# Then I also see a list of each comedian's TV specials' names
+# Where I see a list of each comedian's TV specials' names,
+# I also see each special's run-time length in minutes, and a thumbnail image.
 # 
-# - You may find this information on IMDB.com and other related sites.
-# - Your project does not need to include *every* special the comedian has ever made.
+# - Image locations (URLs) can be stored in the database as a string.
+# - Use the image URLs from IMDB or other online source for the special
+# - Use CSS styling to scale the image smaller if needed to fit on the page
 
 RSpec.describe 'A visitor to our app' do
   it 'should show a list of each comedians specials names' do
     bob = Comedian.create(name: "Bob", age: 30, city: "Denver")
     joe = Comedian.create(name: "Joe", age: 50, city: "Stockholm")
     
-    bob_special = bob.specials.create(name: "This is my TV Special!")
-    joe_special = joe.specials.create(name: "This is Joe's Special")
+    bob_special = bob.specials.create(name: "This is my TV Special!", 
+                                      length: 100,
+                                      image_location: "https://en.wikipedia.org/wiki/Guinea_pig#/media/File:Two_Adult_Guinea_Pigs_(cropped).jpg")
+    joe_special = joe.specials.create(name: "This is Joe's Special",
+                                      length: 60,
+                                      image_location: "https://en.wikipedia.org/wiki/Guinea_pig#/media/File:Cat_and_guinea_pigs.jpg")
     
     visit '/comedians'
     
     within "#Bob" do
       expect(page).to have_content(bob_special.name)
+      expect(page).to have_content("Runtime: #{bob_special.length}")
+      expect(page).to have_css("img[src='#{bob_special.image_location}']")
     end
     within "#Joe" do
       expect(page).to have_content(joe_special.name)
