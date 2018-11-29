2c9354eeefb67f189c6ab4352b0c869170e10117
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-29 15:35:48 -0700

    Adds filtered statistics tests

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -3,13 +3,17 @@ class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     if params[:age]
       @comedians = Comedian.filter_by_age(params[:age])
+      @average_age = Comedian.filtered_average_age(params[:age])
+      @average_length = Special.filtered_average_length(params[:age])
+      @cities = Comedian.filtered_unique_cities(params[:age])
+      @total_specials = Special.filtered_total_specials(params[:age])
     else
       @comedians = Comedian.all
+      @average_age = Comedian.average_age
+      @average_length = Special.average_length
+      @cities = Comedian.unique_cities
+      @total_specials = Special.total_specials
     end
-    @average_age = Comedian.average_age
-    @average_length = Special.average_length
-    @cities = Comedian.unique_cities
-    @total_specials = Special.total_specials
     erb :"comedians/index"
   end
   
--- spec/features/user_sees_filtered_comedians_spec.rb
+++ spec/features/user_sees_filtered_comedians_spec.rb
@@ -1,12 +1,15 @@
-# User Story 5
+# User Story 7
 # 
 # As a visitor
 # When I visit `/comedians?age=34`
-# Then I see the list of comedians on the page only shows
-# comedians who match the age criteria.
+# Then I see a list of all comedians with an age of 34
+# Just like a previous User Story, BUT all other statistics 
+# information in the 'Statistics' area of the page should be limited 
+# to reflect only the information about the comedians listed on 
+# the page.
 # 
-# - All other information on the page is still expected to be present
-# - Testing should check that excluded comedians do not show up.
+# - Testing should ensure that calculated statistics are
+#   correct for a limited subset of data
 
 RSpec.describe 'As a visitor to the app' do
   it 'filters the comedians on the page to only show those of indicated age' do
@@ -23,5 +26,39 @@ RSpec.describe 'As a visitor to the app' do
     expect(page).to have_content("Age: #{joe.age}")
     expect(page).to_not have_content("#{bob.name}")
     expect(page).to_not have_content("#{jane.name}")
+  end
+  
+  it 'filters the statistics to show for only those of indicated age' do
+    bob = Comedian.create(name: "Bob", age: 30, city: "Denver")
+    joe = Comedian.create(name: "Joe", age: 34, city: "Stockholm")
+    jane = Comedian.create(name: "Jane", age: 40, city: "Stockholm")
+    mary = Comedian.create(name: "Mary", age: 34, city: "New York City")
+    bob.specials.create(name: "This is my TV Special!", 
+                                      length: 100,
+                                      image_location: "")
+    joe.specials.create(name: "This is Joe's Special",
+                                      length: 60,
+                                      image_location: "")
+    mary.specials.create(name: "This is Marys Special",
+                                      length: 80,
+                                      image_location: "")
+    joe.specials.create(name: "This is Joe's SECOND Special",
+                                      length: 100,
+                                      image_location: "")
+                                      
+    visit '/comedians?age=34'
+    
+    filtered_average_age = 34
+    filtered_average_length = 80
+    filtered_cities = 'Denver, New York City'
+    filtered_total_specials = 2
+    
+    within "#statistics" do
+      expect(page).to have_content("Average Age: #{filtered_average_age}")
+      expect(page).to have_content("Average TV Special Runtime: #{filtered_average_length}")
+      expect(page).to have_content("Cities: #{filtered_cities}")
+      expect(page).to have_content("Total TV Specials: #{filtered_total_specials}")
+    end
+    
   end
 end
